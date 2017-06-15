a6300b85c7073d753ab5d4299f1110c668150940
Jimi <Jimi@DESKTOP-PM0BUSB.station>
2017-06-15 20:19:10 +0200

    Improved help and flaghelp readability

--- src/main/java/com/universeguard/commands/RegionFlagHelpExecutor.java
+++ src/main/java/com/universeguard/commands/RegionFlagHelpExecutor.java
@@ -24,64 +24,64 @@ public class RegionFlagHelpExecutor implements CommandExecutor {
 			}
 			if(page <= 1) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(1/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "build -", TextColors.RESET, "Sets if non-members can place/break blocks in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "pvp -", TextColors.RESET, "Sets if players can pvp in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "mobdamage -", TextColors.RESET, "Sets if mobs can deal damage to players in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "expdrop -", TextColors.RESET, "Sets if mobs can drop experience in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "creeperexplosions -", TextColors.RESET, "Sets if creepers will break blocks in the region when they explode");	
+				Utils.sendMessage(player, TextColors.YELLOW, "build -", TextColors.WHITE, " Sets if non-members can place/break blocks in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "pvp -", TextColors.WHITE, " Sets if players can pvp in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "mobdamage -", TextColors.WHITE, " Sets if mobs can deal damage to players in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "expdrop -", TextColors.WHITE, " Sets if mobs can drop experience in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "creeperexplosions -", TextColors.WHITE, " Sets if creepers will break blocks in the region when they explode");	
 			}
 			if(page == 2) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(2/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "otherexplosions -", TextColors.RESET, "Sets if explosions will break blocks in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "endermangrief -", TextColors.RESET, "Sets if enderman are allowed to break/place blocks in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "enderpearl -", TextColors.RESET, "Sets if non-members are allowed to use enderpearls in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "enderdragonblockdamage -", TextColors.RESET, "Sets if the enderdragon will break blocks in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "sleep -", TextColors.RESET, "Sets if non-members are allowed to sleep in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "otherexplosions -", TextColors.WHITE, " Sets if explosions will break blocks in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "endermangrief -", TextColors.WHITE, " Sets if enderman are allowed to break/place blocks in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "enderpearl -", TextColors.WHITE, " Sets if non-members are allowed to use enderpearls in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "enderdragonblockdamage -", TextColors.WHITE, " Sets if the enderdragon will break blocks in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "sleep -", TextColors.WHITE, " Sets if non-members are allowed to sleep in the region");
 			}
 			if(page == 3) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(3/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "tnt -", TextColors.RESET, "Sets if tnt explosion will break blocks in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "lighter -", TextColors.RESET, "Sets if non-members are allowed to use lighter in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "chests -", TextColors.RESET, "Sets if non-members are allowed to open chests/trapped chests in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "waterflow -", TextColors.RESET, "Sets if water can flow in the region. This also enable/disable water buckets at all");
-				Utils.sendMessage(player, TextColors.YELLOW, "lavaflow -", TextColors.RESET, "Sets if lava can flow in the region. This also enable/disable lava buckets at all");
+				Utils.sendMessage(player, TextColors.YELLOW, "tnt -", TextColors.WHITE, " Sets if tnt explosion will break blocks in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "lighter -", TextColors.WHITE, " Sets if non-members are allowed to use lighter in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "chests -", TextColors.WHITE, " Sets if non-members are allowed to open chests/trapped chests in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "waterflow -", TextColors.WHITE, " Sets if water can flow in the region. This also enable/disable water buckets at all");
+				Utils.sendMessage(player, TextColors.YELLOW, "lavaflow -", TextColors.WHITE, " Sets if lava can flow in the region. This also enable/disable lava buckets at all");
 			}
 			if(page == 4) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(4/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "use -", TextColors.RESET, "Sets if non-members are allowed to use containers in the region. This will not consider crafting tables, anvils and enchanting tables");
-				Utils.sendMessage(player, TextColors.YELLOW, "vehicleplace -", TextColors.RESET, "Sets if non-members are allowed to place vehicles in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "vehicledestroy -", TextColors.RESET, "Sets if non-members are allowed to destroy vehicles in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "leafdecay -", TextColors.RESET, "Sets if leaves will decay in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "sendchat -", TextColors.RESET, "Sets if non-members are allowed to send chat messages while in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "use -", TextColors.WHITE, " Sets if non-members are allowed to use containers in the region. This will not consider crafting tables, anvils and enchanting tables");
+				Utils.sendMessage(player, TextColors.YELLOW, "vehicleplace -", TextColors.WHITE, " Sets if non-members are allowed to place vehicles in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "vehicledestroy -", TextColors.WHITE, " Sets if non-members are allowed to destroy vehicles in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "leafdecay -", TextColors.WHITE, " Sets if leaves will decay in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "sendchat -", TextColors.WHITE, " Sets if non-members are allowed to send chat messages while in the region");
 			}
 			if(page == 5) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(5/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "potionsplash -", TextColors.RESET, "Sets if non-members are allowed to thrown potions in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "falldamage -", TextColors.RESET, "Sets if players can take fall damage in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "walldamage -", TextColors.RESET, "Sets if players can take suffocate in walls in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "drown -", TextColors.RESET, "Sets if players can drown in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "cantp -", TextColors.RESET, "Sets if players can use the /region teleport command while in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "potionsplash -", TextColors.WHITE, " Sets if non-members are allowed to thrown potions in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "falldamage -", TextColors.WHITE, " Sets if players can take fall damage in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "walldamage -", TextColors.WHITE, " Sets if players can take suffocate in walls in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "drown -", TextColors.WHITE, " Sets if players can drown in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "cantp -", TextColors.WHITE, " Sets if players can use the /region teleport command while in the region");
 			}
 			if(page == 6) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(6/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "canspawn -", TextColors.RESET, "Sets if players can use the /region spawn command while in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "hunger -", TextColors.RESET, "Sets if players will loose hunger points while in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "enderchests -", TextColors.RESET, "Sets if non-members can open enderchests in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "animals -", TextColors.RESET, "Sets if non-hostile mobs can spawn in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "mobs -", TextColors.RESET, "Sets if hostile mobs can spawn in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "canspawn -", TextColors.WHITE, " Sets if players can use the /region spawn command while in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "hunger -", TextColors.WHITE, " Sets if players will loose hunger points while in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "enderchests -", TextColors.WHITE, " Sets if non-members can open enderchests in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "animals -", TextColors.WHITE, " Sets if non-hostile mobs can spawn in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "mobs -", TextColors.WHITE, " Sets if hostile mobs can spawn in the region");
 			}
 			if(page == 7) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(7/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "tntdamage -", TextColors.RESET, "Sets if players will get damage from tnt explosions");
-				Utils.sendMessage(player, TextColors.YELLOW, "otherexplosionsdamage -", TextColors.RESET, "Sets if players will get damage from other explosions");
-				Utils.sendMessage(player, TextColors.YELLOW, "invincible -", TextColors.RESET, "Sets if players are invincible in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "cactusdamage -", TextColors.RESET, "Sets if player will receive damage from cactus in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "firedamage -", TextColors.RESET, "Sets if player will receive damage from fire/lava in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "tntdamage -", TextColors.WHITE, " Sets if players will get damage from tnt explosions");
+				Utils.sendMessage(player, TextColors.YELLOW, "otherexplosionsdamage -", TextColors.WHITE, " Sets if players will get damage from other explosions");
+				Utils.sendMessage(player, TextColors.YELLOW, "invincible -", TextColors.WHITE, " Sets if players are invincible in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "cactusdamage -", TextColors.WHITE, " Sets if player will receive damage from cactus in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "firedamage -", TextColors.WHITE, " Sets if player will receive damage from fire/lava in the region");
 			}
 			if(page >= 8) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Flags Help(8/8)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "mobpvp -", TextColors.RESET, "Sets if players can damage mobs in the region");
-				Utils.sendMessage(player, TextColors.YELLOW, "animalspvp -", TextColors.RESET, "Sets if players can damage animals in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "mobpvp -", TextColors.WHITE, " Sets if players can damage mobs in the region");
+				Utils.sendMessage(player, TextColors.YELLOW, "animalspvp -", TextColors.WHITE, " Sets if players can damage animals in the region");
 				Utils.sendMessage(player, TextColors.GOLD, "--------------------------------------------");
 			}
 
--- src/main/java/com/universeguard/commands/RegionHelpExecutor.java
+++ src/main/java/com/universeguard/commands/RegionHelpExecutor.java
@@ -24,40 +24,40 @@ public class RegionHelpExecutor implements CommandExecutor {
 			}
 			if(page <= 1) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Help(1/5)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region -", TextColors.RESET, "", TextColors.RESET, "Get the region selector item");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region save -", TextColors.RESET, "Save the current editing region");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region info (region) -", TextColors.RESET, "Display the details of the region at the current location or the specified one");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region delete (region) -", TextColors.RESET, "Delete the region at the current location or the specified one");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region name [name] -", TextColors.RESET, "Rename the current editing region with the specified name");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region -", TextColors.WHITE, " ", TextColors.WHITE, " Get the region selector item");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region save -", TextColors.WHITE, " Save the current editing region");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region info (region) -", TextColors.WHITE, " Display the details of the region at the current location or the specified one");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region delete (region) -", TextColors.WHITE, " Delete the region at the current location or the specified one");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region name [name] -", TextColors.WHITE, " Rename the current editing region with the specified name");
 			}
 			if(page == 2) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Help(2/5)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region list -", TextColors.RESET, "Show the names of all the saved regions");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region gamemode [gamemode] -", TextColors.RESET, "Set the gamemode of the current editing region");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region edit (region) -", TextColors.RESET, "Allows to edit the region at the current location or the specified one");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region flag [flag] [value] -", TextColors.RESET, "Set the flag value for the current editing region");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region add [player] (region) -", TextColors.RESET, "Add a owner to the region at the current location or the specified one");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region list -", TextColors.WHITE, " Show the names of all the saved regions");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region gamemode [gamemode] -", TextColors.WHITE, " Set the gamemode of the current editing region");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region edit (region) -", TextColors.WHITE, " Allows to edit the region at the current location or the specified one");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region flag [flag] [value] -", TextColors.WHITE, " Set the flag value for the current editing region");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region add [player] (region) -", TextColors.WHITE, " Add a owner to the region at the current location or the specified one");
 			}
 			if(page == 3) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Help(3/5)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region remove [player] (region) -", TextColors.RESET, "Remove a owner to the region at the current location or the specified one");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region global [flag] [value] -", TextColors.RESET, "Set the value of the global flag (used outside of regions)");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region globallist -", TextColors.RESET, "Displays all global flag values");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region setteleport [x] [y] [z] -", TextColors.RESET, "Set the teleport location of the current editing regiont at the specified coordinates");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region setspawn [x] [y] [z] -", TextColors.RESET, "Set the spawn location of the current editing regiont at the specified coordinates");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region remove [player] (region) -", TextColors.WHITE, " Remove a owner to the region at the current location or the specified one");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region global [flag] [value] -", TextColors.WHITE, " Set the value of the global flag (used outside of regions)");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region globallist -", TextColors.WHITE, " Displays all global flag values");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region setteleport [x] [y] [z] -", TextColors.WHITE, " Set the teleport location of the current editing regiont at the specified coordinates");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region setspawn [x] [y] [z] -", TextColors.WHITE, " Set the spawn location of the current editing regiont at the specified coordinates");
 				
 			}
 			if(page == 4) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Help(4/5)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region teleport [region] -", TextColors.RESET, "Set the player at the teleport location of the specified region");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region spawn [region] -", TextColors.RESET, "Set the player at the spawn location of the specified region");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region priority [value] -", TextColors.RESET, "Set the priority of the current editing region");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region command [command] (region) -", TextColors.RESET, "Allow or disallow a command in the current editing region or the specified one");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region help -", TextColors.RESET, "Displays this list");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region teleport [region] -", TextColors.WHITE, " Set the player at the teleport location of the specified region");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region spawn [region] -", TextColors.WHITE, " Set the player at the spawn location of the specified region");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region priority [value] -", TextColors.WHITE, " Set the priority of the current editing region");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region command [command] (region) -", TextColors.WHITE, " Allow or disallow a command in the current editing region or the specified one");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region help -", TextColors.WHITE, " Displays this list");
 			}
 			if(page >= 5) {
 				Utils.sendMessage(player, TextColors.GOLD, "------------------Help(5/5)------------------");
-				Utils.sendMessage(player, TextColors.YELLOW, "/region flaghelp -", TextColors.RESET, "Displays the flags help");
+				Utils.sendMessage(player, TextColors.YELLOW, "/region flaghelp -", TextColors.WHITE, " Displays the flags help");
 				Utils.sendMessage(player, TextColors.GOLD, "--------------------------------------------");
 			}
 			
